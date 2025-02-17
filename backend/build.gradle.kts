97c2e1183ec2b2c3934f2b6493e010521f89e237
jhs512 <jangka512@gmail.com>
2025-02-18 02:44:52 +0900

    java -> kotlin on build.gradle

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.4.1"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -24,6 +25,10 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
