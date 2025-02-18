136ace9b9d71385a81c98dd8f46b4b4fed41e515
jhs512 <jangka512@gmail.com>
2025-02-18 09:13:17 +0900

    work

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.4.1"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
